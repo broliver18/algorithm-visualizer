e45fddeebe9c3d7773c3f2b36dd059c3f6dcaca4
broliver18 <bruno.olive504@gmail.com>
2023-10-18 22:27:15 -0400

    Make SVGs appear on Safari

--- src/Components/Icons/StartIcon.js
+++ src/Components/Icons/StartIcon.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function StartIcon() {
   return (
-    <svg fill="#420863" viewBox="240 0 500 1030" xmlns="http://www.w3.org/2000/svg">
+    <svg fill="#420863" width="98%" viewBox="240 0 500 1030" xmlns="http://www.w3.org/2000/svg">
       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M419.3 264.8l-61.8 61.8L542.9 512 357.5 697.4l61.8 61.8L666.5 512z"></path></g>
     </svg>
   )
